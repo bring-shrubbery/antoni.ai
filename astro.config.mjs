a15fdaf6de1bb900167c1f939947611bec85a35e
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2022-10-19 13:50:13 +0300

    Image and SEO updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,18 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import partytown from "@astrojs/partytown";
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://antoni.ai/",
   integrations: [
     solidJs(),
     tailwind(),
     image(),
     sitemap(),
     prefetch(),
-    partytown(),
+    // partytown(),
   ],
   output: "static",
   // adapter: vercel()
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,5 @@
 import type { Component } from "solid-js";
+import { Image } from "@astrojs/image/components";
 
 export type IconType =
   | "address-card"
