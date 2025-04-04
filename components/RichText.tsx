844bb6a31310db97d2f9175a0f35e91f8b8919df
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2025-04-04 15:31:03 +0300

    Fix rich text lists

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -1,24 +1,33 @@
 import { memo } from "react";
-import Markdown from 'react-markdown';
+import Markdown from "react-markdown";
 
 type RichTextProps = {
-  text: string,
+  text: string;
 };
 
-const Link: React.FC<React.JSX.IntrinsicElements['a']> = memo(({ href, children }) => {
-  return <a href={href} target='_blank'>{children}</a>;
-});
+const Link: React.FC<React.JSX.IntrinsicElements["a"]> = memo(
+  ({ href, children }) => {
+    return (
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    );
+  }
+);
 
-const components: Partial<{ [TagName in keyof React.JSX.IntrinsicElements]: React.FunctionComponent<React.JSX.IntrinsicElements[TagName]> }> = {
+const components: Partial<{
+  [TagName in keyof React.JSX.IntrinsicElements]: React.FunctionComponent<
+    React.JSX.IntrinsicElements[TagName]
+  >;
+}> = {
   a: Link,
+  ul: ({ children }) => <ul className="list-disc pl-5">{children}</ul>,
+  ol: ({ children }) => <ol className="list-decimal pl-5">{children}</ol>,
+  li: ({ children }) => <li className="pl-2">{children}</li>,
 };
 
-const RichText: React.FC<RichTextProps> = ({
-  text
-}) => {
-  return (
-    <Markdown components={components as any}>{text}</Markdown>
-  )
-}
+const RichText: React.FC<RichTextProps> = ({ text }) => {
+  return <Markdown components={components as any}>{text}</Markdown>;
+};
 
 export default RichText;
