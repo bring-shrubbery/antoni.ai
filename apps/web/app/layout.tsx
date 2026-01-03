cd2d7c9de63c15fba69a96c5b1f503e224610a6d
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-01-03 16:01:00 +0200

    Updated font to Lexend, and added umami script

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { loadCV } from "@/lib/loadCV";
 import { ThemeProvider } from "next-themes";
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import { loadCV } from "@/lib/loadCV";
 import { cn } from "@/lib/utils";
-import PlausibleProvider from "next-plausible";
 import Script from "next/script";
 
-const inter = Inter({
+import "./globals.css";
+
+const fontSans = Lexend({
   subsets: ["latin"],
   variable: "--font-sans",
 });
@@ -35,14 +35,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(inter.variable, "font-sans")}>
-        <PlausibleProvider domain="antoni.cv">
-          <ThemeProvider attribute="class">{children}</ThemeProvider>
-        </PlausibleProvider>
+      <body className={cn(fontSans.variable, "font-sans")}>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
         <Script
           src="https://onedollarchatbot.com/script/g492j4c474wkzzcp5enj0"
           strategy="lazyOnload"
         />
+        <script
+          defer
+          src="https://analytics.quassum.com/script.js"
+          data-website-id="5c8cdbba-eb31-4b71-a459-5405bf845521"
+        ></script>
       </body>
     </html>
   );
