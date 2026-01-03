18701158a7e5a36b32121026d31e07daecacbf46
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-01-03 20:16:58 +0200

    Revert "Vercel setup"
    This reverts commit 73e3d333642223ffd35cf5f1befb1e631d6a8e1f.

--- apps/web/src/entry-client.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference types="vinxi/types/client" />
-import { hydrateRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start";
-import { getRouter } from "./router";
-
-const router = getRouter();
-
-hydrateRoot(document.getElementById("root")!, <StartClient router={router} />);
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart({
-      deployment: {
-        preset: "vercel",
-      },
       prerender: {
         enabled: true,
         onSuccess: ({ page }) => {
